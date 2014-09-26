6ef3edd11aa8e75bd57decb12d3227e2f678a943
EricLiu <liuweiccy@users.noreply.github.com>
2014-09-26 20:36:12 +0800

    redis源码

---
+++ redis/src/strings.c
@@ -0,0 +1,10 @@
+#define BLURE "Authentic imitation! "
+
+int main()
+{
+	printf("*%2s*\n",BLURE);
+	printf("*%24s*\n",BLURE);
+	printf("*%24.5s*\n",BLURE);
+	printf("*%-24.5s*\n",BLURE);
+	return 0;
+}
---
+++ redis/src/width.c
@@ -0,0 +1,10 @@
+#define PAGES 931
+
+int main()
+{
+	printf("*%d*\n",PAGES);
+	printf("*%2d*\n",PAGES);
+	printf("*%10d*\n",PAGES);
+	printf("*%-10d*\n",PAGES);
+	return 0;
+}
